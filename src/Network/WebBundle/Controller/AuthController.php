24075801dab3be14a58661d55d26b5e8168238ec
Mark Tertishniy <mtertishniy@gmail.com>
2014-09-21 12:10:53 +1100

    Add base for AuthController

---
+++ src/Network/WebBundle/Controller/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Network\WebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AuthController extends Controller
+{
+    public function vkLoginAction()
+    {
+
+    }
+
+    public function gplusLoginAction()
+    {
+
+    }
+
+    public function steamLoginAction()
+    {
+
+    }
+
+    public function facebookLoginAction()
+    {
+
+    }
+
+    public function instagramLoginAction()
+    {
+
+    }
+
+}
