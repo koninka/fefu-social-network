b085cd898f8a03dc4dcb9db42ae57b2b7a3ecb14
monoklid <artem94@bk.ru>
2014-10-16 19:48:06 +0400

    Add base for enum DBAL type

---
+++ src/Network/StoreBundle/DBAL/EnumType.php
@@ -0,0 +1,47 @@
+<?php
+namespace Network\StoreBundle\DBAL;
+
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+abstract class EnumType extends Type
+{
+    protected $name;
+    protected $values = array();
+
+    public function getSqlDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        $values = array_map(function($val) { return "'".$val."'"; }, $this->values);
+
+        return "ENUM(".implode(", ", $values).") COMMENT '(DC2Type:".$this->name.")'";
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return $value;
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (!in_array($value, $this->values)) {
+            throw new \InvalidArgumentException("Invalid '".$this->name."' value.");
+        }
+
+        return $value;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getChoices()
+    {
+        $choices = [];
+        foreach ($this->values as $value) {
+            $choices[$value] = ucfirst($value);
+        }
+
+        return $choices;
+    }
+}
