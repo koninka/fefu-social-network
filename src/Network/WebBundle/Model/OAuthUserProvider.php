9c396322f6e97860744b3011b6340cca23a6e2c6
Apanasenko <alexander.apanasenko@gmail.com>
2014-10-24 21:15:48 +1100

    Redirect to api when authorize with VK

---
+++ src/Network/WebBundle/Model/OAuthUserProvider.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Network\WebBundle\Model;
+
+use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthUserProvider as BaseOAuthUserProvider;
+
+class OAuthUserProvider extends BaseOAuthUserProvider { }
