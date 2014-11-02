29a39510b14c4c4fa4f52a1f109a56de873195f7
Michail Zinov <zinov.m.v@gmail.com>
2014-11-02 15:46:41 +1000

    Add command for generating symlink points to a public files

--- update_project.sh
+++ update_project.sh
@@ -22,6 +22,7 @@ php app/console doctrine:database:create
 php app/console doctrine:generate:entities --no-backup Network
 php app/console doctrine:schema:create
 php app/console doctrine:fixtures:load
+php app/console assets:install web --symlink
 
 echo -n "Set the environment name for the cache clearing (\"prod\" or \"dev\", default: \"dev\"): "
 read env
