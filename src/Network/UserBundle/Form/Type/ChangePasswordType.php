179e79d31e612c4c5d606660fabf2d0f7af9403d
Anton Zlotnikov <exp.razor@gmail.com>
2014-11-03 21:38:46 +1000

    Add password resetting with email confirmation

--- src/Network/UserBundle/Form/Type/ChangePasswordType.php
+++ src/Network/UserBundle/Form/Type/ChangePasswordType.php
@@ -4,7 +4,7 @@ namespace Network\UserBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+use FOS\UserBundle\Form\Type\ChangePasswordFormType as BaseType;
 
 class ChangePasswordType extends BaseType
 {
