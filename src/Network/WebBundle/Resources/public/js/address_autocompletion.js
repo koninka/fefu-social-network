852e974bbd6dab393fe49c8bda713edd99f25cd4
GuraYaroslava <yaroslava_05@mail.ru>
2014-11-06 22:05:58 +1000

    Add jQuery Kladr with style

---
+++ src/Network/WebBundle/Resources/public/js/address_autocompletion.js
@@ -0,0 +1,50 @@
+var autocomplete = function() {
+
+    var token = '51dfe5d42fb2b43e3300006e';
+    var key   = '86a2c2a06f1b2451a87d05512cc2c3edfdf41969';
+
+    var city     = $('[id$="city"]');
+    var street   = $('[id$="street"]');
+    var building = $('[id$="building"]');
+
+    city.kladr({
+        token: token,
+        key: key,
+        type: $.kladr.type.city,
+        select: function(obj) {
+            street.kladr('parentType', $.kladr.type.city);
+            street.kladr('parentId', obj.id);
+            building.kladr('parentType', $.kladr.type.city);
+            building.kladr('parentId', obj.id);
+        }
+    });
+
+    street.kladr({
+        token: token,
+        key: key,
+        type: $.kladr.type.street,
+        select: function(obj) {
+            building.kladr('parentType', $.kladr.type.street);
+            building.kladr('parentId', obj.id);
+        }
+    });
+
+    building.kladr({
+        token: token,
+        key: key,
+        type: $.kladr.type.building
+    });
+
+};
+
+$(document).ready(function() {
+
+    autocomplete();
+
+    $('span[id$="idAddress"]').click(function(event) {
+        setTimeout(function () {
+            autocomplete();
+        }, 2000)
+    });
+
+});
