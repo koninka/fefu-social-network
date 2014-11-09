ad005da33a61d2c7dafa519d4c1a9ee334150db0
Anton Zlotnikov <exp.razor@gmail.com>
2014-11-09 21:58:00 +1000

    Remove unnecessary entities generation in update_project.sh

--- update_project.sh
+++ update_project.sh
@@ -19,7 +19,7 @@ fi
 
 php app/console doctrine:database:drop --force
 php app/console doctrine:database:create
-php app/console doctrine:generate:entities --no-backup Network
+#php app/console doctrine:generate:entities --no-backup Network
 php app/console doctrine:schema:create
 php app/console doctrine:fixtures:load
 php app/console assets:install web --symlink
