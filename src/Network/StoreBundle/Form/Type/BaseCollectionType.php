e8ef1a68958f9d83131d9aea10330e0271a8dd1f
monoklid <artem94@bk.ru>
2014-11-24 14:51:58 +0400

    Add Network\StoreBundle\Form\Type\BaseCollectionType
    This type is little wrapper to extract editing such fields as jobs,
    education to another pages.

---
+++ src/Network/StoreBundle/Form/Type/BaseCollectionType.php
@@ -0,0 +1,35 @@
+<?php
+namespace Network\StoreBundle\Form\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+
+class BaseCollectionType extends CollectionType
+{
+
+    protected $baseType = null;
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            $this->baseType . 's',
+            'collection',
+            [
+                'type' => $this->baseType,
+                'required' => false,
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+            ]
+        );
+        $builder->add('save', 'submit');
+    }
+
+    public function getName()
+    {
+        return 'network_storebundle_base_collection';
+    }
+
+}
