b0a93fc073bb3d827636f054b5ef1fe11522f09e
Michail Zinov <zinov.m.v@gmail.com>
2014-11-28 06:52:44 +1000

    Change register/login template and style

--- src/Network/UserBundle/Service/UserFormBuilder.php
+++ src/Network/UserBundle/Service/UserFormBuilder.php
@@ -11,9 +11,9 @@ class UserFormBuilder
     static function baseBuildForm(FormBuilderInterface $builder)
     {
         $builder->remove('username')
-            ->add('firstName', null, ['label' => 'Имя'])
-            ->add('lastName', null, ['label' => 'Фамилия'])
-            ->add('gender', 'choice', ['label' => 'Пол', 'choices' => Type::getType('genderEnumType')->getChoices()]);
+            ->add('firstName', 'text', ['attr' => ['placeholder' => 'Имя',],'label' => false])
+            ->add('lastName', 'text', ['attr' => ['placeholder' => 'Фамилия',],'label' => false])
+            ->add('gender', 'choice', ['label' => false, 'choices' => Type::getType('genderEnumType')->getChoices()]);
     }
 
 }
