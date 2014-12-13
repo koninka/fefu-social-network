9718949ba357d713cb4e803fedafdef5d0f093b6
Khoschenko Artem <artem94@bk.ru>
2014-12-13 13:39:11 +1000

    Refactor datepicker.js
    Create initDatePickers() with single parameter root - container where
    datepickers SHOULD BE searched. Run it when document gets ready.

--- src/Network/WebBundle/Resources/public/js/datepicker.js
+++ src/Network/WebBundle/Resources/public/js/datepicker.js
@@ -1,18 +1,24 @@
-$(function() {
+function initDatePickers(root) {
 
     var currYear = (new Date).getFullYear();
 
     $.datepicker.setDefaults($.datepicker.regional['']);
 
-    $('input[class^="datepicker"]')
-    .attr('type', 'text')
-    .datepicker({
+    var datepickerOptions = {
         showAnim: 'slideDown',
         dateFormat: 'yy-mm-dd',
         changeMonth: true,
         changeYear: true,
         yearRange: String(currYear-120 + ':' + currYear)
-    })
-    .attr('type', 'text');
+    };
 
+    $(root)
+        .find('input[class~="datepicker"]')
+        .each(function(){
+            $(this).datepicker(datepickerOptions);
+        });
+}
+
+$(document).on('ready', function() {
+    initDatePickers(this);
 });
