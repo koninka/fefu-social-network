da66ab12ab1db981131de9512de613680e510290
USKamicadze <uskamicadze@gmail.com>
2015-01-26 16:14:50 +1000

    Refactor
    Transfer some code from ProfileController to
    ThreadController and ImService

--- src/Network/StoreBundle/Service/ImService.php
+++ src/Network/StoreBundle/Service/ImService.php
@@ -2,7 +2,15 @@
 
 namespace Network\StoreBundle\Service;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Acl\Exception\Exception;
+use Network\StoreBundle\Entity\Thread;
+use Network\StoreBundle\Entity\Post;
+use Network\StoreBundle\Entity\User;
+use Network\StoreBundle\DBAL\ThreadEnumType;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ImService
 {
@@ -19,27 +27,131 @@ class ImService
         $this->em = $em;
     }
 
-    public function persistThread($thread)
+    public function getThreadByIdAndUserIdOrThrow($threadId, $userId)
     {
-        $this->em->persist($thread);
+        $threadRepo = $this->em->getRepository('NetworkStoreBundle:Thread');
+        $thread = $threadRepo->getThreadByIdAndUser($threadId, $userId);
+        if ($thread == null) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        return $thread;
+    }
+
+    public function createPost(User $user, Thread $thread, $text)
+    {
+        $oldTimeZone = date_default_timezone_get();
+        date_default_timezone_set("UTC");
+
+        $post = new Post();
+        $post->setText($text)
+            ->setTs(new \DateTime('now'))
+            ->setUser($user)
+            ->setThread($thread);
+        $thread->incUnreadPosts($user);
+
+        $this->persistAndFlush($post);
+
+        date_default_timezone_set($oldTimeZone);
+
+        return $post;
+    }
+
+    public function createDialogOrConference(User $user, ArrayCollection $recipientUsers, $topic)
+    {
+        $recipientsCount = $recipientUsers->count();
+        if ($recipientUsers->count() == 1) {
+            $recipientUser = $recipientUsers[0];
+            $thread = $this->createDialog($user, $recipientUser);
+        } else {
+            $thread = $this->createConference($user, $recipientUsers, $recipientsCount, $topic);
+        }
+
+        return $thread;
+    }
+
+    private function makeTopicFromUsers(ArrayCollection $recipientUsers, $recipientsCount)
+    {
+        $topic = '';
+        foreach ($recipientUsers as $k => $recipientUser) {
+            $topic .= $recipientUser->getFirstName();
+            if ($k + 1 < $recipientsCount) {
+                $topic .= ', ';
+            }
+        }
+
+        return $topic;
+    }
+    private function createConference(User $user, ArrayCollection $recipientUsers, $recipientsCount, $topic)
+    {
+        if (trim($topic) == '') {
+            $topic = $this->makeTopicFromUsers($recipientUsers, $recipientsCount);
+        }
+        $thread = new Thread();
+        $thread
+            ->setTopic($topic)
+            ->setType(ThreadEnumType::T_CONFERENCE);
+        $this->persistAndFlush($thread);
+        foreach ($recipientUsers as $recipientUser) {
+            $thread->addUser($recipientUser);
+        }
+        $thread->addUser($user);
+        $this->persistAndFlush($thread);
+
+        return $thread;
+    }
+
+    private function createDialog(User $user, User $recipientUser)
+    {
+        $thread = $this->em
+            ->getRepository('NetworkStoreBundle:Thread')
+            ->findByUsers($user->getId(), $recipientUser->getId());
+        if (!$thread or count($thread) == 0) {
+            // there's no 1x1 thread between this pair of users
+            // so we're creating a new one
+            $thread = new Thread();
+            $thread->setTopic('no topic');
+            $this->persistAndFlush($thread); //because of foreign key error
+            $thread->addUser($user)->addUser($recipientUser);
+            $this->persistAndFlush($thread);
+
+        } elseif (count($thread) > 1) {
+            throw new Exception('SERVER ERROR: 2 dialogs between 2 persons');
+        } else {
+            $thread = $thread[0];
+        }
+
+        return $thread;
+    }
+
+    public function checkAccessForThread($threadId, $userId)
+    {
+        $threadRepo = $this->em->getRepository('NetworkStoreBundle:Thread');
+        if (!$threadRepo->checkPermission($threadId, $userId)) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+    }
+
+    private function persistAndFlush($entity)
+    {
+        $this->em->persist($entity);
         $this->em->flush();
     }
 
-    public function persistPost($post)
+    public function readPosts($userId, $threadId, $count)
     {
-        $this->em->persist($post);
-        $this->em->flush();
-    }
-    //WTF
-    public function persistUserThread($userThread)
-    {
-        $this->em->persist($userThread);
-        $this->em->flush();
-    }
+        if ($threadId == null) {
+            return new JsonResponse(['error' => 'Invalid thread Id']);
+        }
+        $userThreadRep = $this->em->getRepository('NetworkStoreBundle:UserThread');
+        $userThread = $userThreadRep->findByUserAndThread($userId, $threadId);
+        if ($userThread == null) {
+            return new JsonResponse(['error' => 'thread not found']);
+        }
+        $userThread->decUnreadPosts($count);
+        $this->persistAndFlush($userThread);
 
-    public function getThreadById($threadId)
-    {
-        return $this->em->getRepository('NetworkStoreBundle:Thread')->findOneById($threadId);
+        return new JsonResponse(['count' => $count]);
     }
 
 }
