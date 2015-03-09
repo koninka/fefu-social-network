4ca514f5e763efbeb8e51f6563f86e96e4ae96dc
myhobbymurder <myhobbymurder@gmail.com>
2015-03-09 14:00:38 +1000

    generate CacheBundle with extension

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -42,7 +42,8 @@ class AppKernel extends Kernel
             new Network\OpenIdBundle\NetworkOpenIdBundle(),
             new Fp\OpenIdBundle\FpOpenIdBundle(),
             new Snc\RedisBundle\SncRedisBundle(),
-            new OldSound\RabbitMqBundle\OldSoundRabbitMqBundle()
+            new OldSound\RabbitMqBundle\OldSoundRabbitMqBundle(),
+            new Network\CacheBundle\NetworkCacheBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
